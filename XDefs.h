d33f782831955a9a671ba62cba68853ffc968562
Bozo David <bozo1@dell4.szkp>
2018-11-05 01:38:08 +0100

    Fixed typo

--- XDefs.h
+++ XDefs.h
@@ -3,14 +3,16 @@
 #define BMP_DATA_BEGIN 0x7A
 #define TWO_GB_SIZE 2147483648
 
-#ifdef _MSC_VER && !__INTEL_COMPILER
+#if _MSC_VER && !__INTEL_COMPILER
+	#warning "Using MSC"
 	#define xAllignedAlloc(align, size) _aligned_malloc(size, align)
 	#define xIMG1_PATH "B:\\Korszeru\\korszeru1.bmp"
 	#define xIMG2_PATH "B:\\Korszeru\\korszeru2.bmp"
 #elif __GNUC__
+	#warning "Using GCC"
 	#define xAllignedAlloc(align, size) aligned_alloc(align, size)
-	#define xIMG1_PATH "korszeru/korszeru1.bmp"
-	#define xIMG2_PATH "korszeru/korszeru2.bmp"
+	#define xIMG1_PATH "/korszeru/korszeru1.bmp"
+	#define xIMG2_PATH "/korszeru/korszeru2.bmp"
 #else
 	#error Not supported compiler
 #endif
