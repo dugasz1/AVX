fee8153e3b0961aed0067ac11426f3ce4cc8ccc7
mcaimer2@gmail.com <mcaimer2@gmail.com>
2018-11-05 21:28:55 +0100

    Implemented F2

--- F2/F2.c
+++ F2/F2.c
@@ -12,18 +12,21 @@ int main()
 	result_buff = (float*)xAllignedAlloc(32, 32);
 
 
-	f = fopen("/korszeru/floats.2gb.bin", "rb");
+	f = fopen(xFLOATS_PATH, "rb");
 
 	fread(buffer, TWO_GB_SIZE, 1, f);
 
-	__m256 result = _mm256_xor_ps(result, result);
+	__m256 result = _mm256_setzero_ps();
 	__m256 b = _mm256_load_ps(limit);
-	for (i = 0; i < TWO_GB_SIZE; i += 8)
+	for (i = 0; i < TWO_GB_SIZE / 4; i += 8)
 	{
 		__m256 a = _mm256_load_ps(buffer + i);
+		_mm256_store_ps(result_buff, a);
 		__m256 mask = _mm256_cmp_ps(a, b, _CMP_GT_OQ);
+		_mm256_store_ps(result_buff, mask);
 		__m256 greather = _mm256_and_ps(a, mask);
-		__m256 result = _mm256_and_ps(result, greather);
+		_mm256_store_ps(result_buff, a);
+		result = _mm256_add_ps(result, greather);
 	}
 	_mm256_store_ps(result_buff, result);
 	for (i = 0; i < 8; i++)
--- XDefs.h
+++ XDefs.h
@@ -14,11 +14,13 @@
 	#define xAllignedAlloc(align, size) _aligned_malloc(size, align)
 	#define xIMG1_PATH "B:\\Korszeru\\korszeru1.bmp"
 	#define xIMG2_PATH "B:\\Korszeru\\korszeru2.bmp"
+	#define xFLOATS_PATH "B:\\Korszeru\\floats.2gb.bin"
 #elif __GNUC__
 	#warning "Compiler: GCC"
 	#define xAllignedAlloc(align, size) aligned_alloc(align, size)
 	#define xIMG1_PATH "/korszeru/korszeru1.bmp"
 	#define xIMG2_PATH "/korszeru/korszeru2.bmp"
+	#define xFLOATS_PATH "/korszeru/floats.2gb.bin"
 #else
 	#error Not supported compiler
 #endif
